fbdb60364399fb6f1b9f4ca3eae2c7775b16ca0a
Zyussx <andrealex30012008@gmail.com>
2026-05-14 15:20:13 -0300

    corrige cors

--- main.py
+++ main.py
@@ -21,7 +21,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://frontend-chi-flame-33.vercel.app",
+        "https://orquestra-frontend.vercel.app",
+        "http://localhost:3000"
+    ],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Conta(BaseModel):
     codigo: str = ""
